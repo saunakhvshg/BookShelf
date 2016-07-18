19c91f669ee5c0b35a25b8cc86af8954ef201158
saunak <saunak@operamediaworks.com>
2016-07-18 16:44:22 +0530

    BookShelf
    First commit. Commit till the creation of home page.
    Where on clicking the login button we are taken to the home screen. The
    home screen is still not functional

---
+++ app/src/main/java/com/example/saunakc/bookshelf/UI/Login/Login.java
@@ -0,0 +1,51 @@
+package com.example.saunakc.bookshelf.UI.Login;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.saunakc.bookshelf.R;
+import com.example.saunakc.bookshelf.UI.ForgotPassword.ForgotPassword;
+import com.example.saunakc.bookshelf.UI.Home.HomePage;
+
+public class
+Login extends AppCompatActivity {
+
+    private EditText emailId;
+    private EditText passWord;
+    private Button signIn;
+    private TextView forgotPasswd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getSupportActionBar().hide();
+        emailId=(EditText)findViewById(R.id.et_emailId);
+        passWord=(EditText)findViewById(R.id.et_password);
+        signIn=(Button)findViewById(R.id.bt_signIn);
+        forgotPasswd=(TextView)findViewById(R.id.tv_forgotPassword);
+
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, HomePage.class));
+            }
+        });
+
+        forgotPasswd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, ForgotPassword.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/saunakc/bookshelf/UI/SplashScreen/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.saunakc.bookshelf.UI.SplashScreen;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.example.saunakc.bookshelf.R;
+import com.example.saunakc.bookshelf.UI.Login.Login;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+
+        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        Handler mainThread = new Handler();
+        mainThread.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent loginIntent=new Intent(MainActivity.this, Login.class);
+                startActivity(loginIntent);
+            }
+        },3000);
+
+
+    }
+
+}
